ec940766faf664aab73de32a8f866b27c3d2fe4e
AcidityClub <jgreid1027@aol.com>
2025-02-08 19:15:00 -0500

    Update ReachNotifier.java

--- java/net/hypixel/lynx/anticheat/ReachNotifier.java
+++ java/net/hypixel/lynx/anticheat/ReachNotifier.java
@@ -3,10 +3,14 @@ package net.hypixel.lynx.anticheat;
 import net.hypixel.lynx.Lynx;
 import net.hypixel.lynx.chat.ChatFacade;
 import net.hypixel.lynx.util.Util;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.util.ChatComponentText;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.entity.living.LivingEvent;
+import net.minecraftforge.event.entity.living.LivingHurtEvent;
 import net.minecraftforge.event.entity.player.AttackEntityEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.network.FMLNetworkEvent;
 
 public class ReachNotifier {
     public ReachNotifier() {
@@ -14,17 +18,31 @@ public class ReachNotifier {
     }
 
     @SubscribeEvent
-    public void onAttack(AttackEntityEvent e) {
-        if (Lynx.isTargeting() && Lynx.getTargetingUI() != null && Lynx.getTargetingUI().getTarget() != null && e.entity == Lynx.getTargetingUI().getTarget()) {
+    public void onHurt(LivingHurtEvent e) {
+        EntityPlayer target = null;
+        EntityPlayer entity = null;
+
+        Util.out(e.source.getDamageType() + " | " + e.entity.getName());
+
+        if (e.source != null && e.source.getEntity() instanceof EntityPlayer) {
+            entity = (EntityPlayer) e.source.getEntity();
+        }
+        if (e.entity instanceof EntityPlayer) {
+            target = (EntityPlayer) e.entity;
+        }
+
+        if (entity == null || target == null) return;
+
+        if (Lynx.isTargeting() && Lynx.getTargetingUI() != null && Lynx.getTargetingUI().getTargetName() != null && entity.getName() == Lynx.getTargetingUI().getTargetName()) {
             if (!Lynx.get().isDoingReachCheckForTarget()) return;
 
-            double dist = e.entity.getDistanceToEntity(e.target);
+            double dist = entity.getDistanceToEntity(target);
 
             if (dist >= 3.3) {
-                ChatFacade.get().printChatMessage(new ChatComponentText("\u00A73[TMU / !] \u00A7b" + e.entity.getName() + " \u00A73attacked \u00A7b" + e.target.getName() + " \u00A73from \u00A79" + dist + " blocks\u00A73."));
+                ChatFacade.get().printChatMessage(new ChatComponentText("\u00A73[TMU / !] \u00A7b" + entity.getName() + " \u00A73attacked \u00A7b" + target.getName() + " \u00A73from \u00A79" + dist + " blocks\u00A73."));
             }
             else {
-                ChatFacade.get().printChatMessage(new ChatComponentText("\u00A73[TMU / ?] \u00A7b" + e.entity.getName() + " \u00A73attacked \u00A7b" + e.target.getName() + " \u00A73from \u00A79" + dist + " blocks\u00A73."));
+                ChatFacade.get().printChatMessage(new ChatComponentText("\u00A73[TMU / ?] \u00A7b" + entity.getName() + " \u00A73attacked \u00A7b" + target.getName() + " \u00A73from \u00A79" + dist + " blocks\u00A73."));
             }
         }
     }
